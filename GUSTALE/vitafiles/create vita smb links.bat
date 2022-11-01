e992697246bb1c2b3f82ee14fff00bf34d8e924d
RKReid <robkreid05@gmail.com>
2022-11-01 11:22:43 -0400

    save update

---
+++ GUSTALE/vitafiles/create vita smb links.bat
@@ -0,0 +1,4 @@
+mklink /d "C:\Users\robkr\AppData\Roaming\Vita3K\Vita3K\ux0\app\GUSE01116\sce_module" "C:\Users\robkr\Documents\GitHub\GUSTALE\GUSTALE\vitafiles\sce_module"
+mklink /d "C:\Users\robkr\AppData\Roaming\Vita3K\Vita3K\ux0\app\GUSE01116\sce_sys" "C:\Users\robkr\Documents\GitHub\GUSTALE\GUSTALE\vitafiles\sce_sys"
+mklink "C:\Users\robkr\AppData\Roaming\Vita3K\Vita3K\ux0\app\GUSE01116\eboot.bin" "C:\Users\robkr\Documents\GitHub\GUSTALE\GUSTALE\vitafiles\eboot.bin"
+mklink /d "C:\Users\robkr\AppData\Roaming\Vita3K\Vita3K\ux0\app\GUSE01116\games" "C:\Users\robkr\Documents\GitHub\GUSTALE\GUSTALE"
